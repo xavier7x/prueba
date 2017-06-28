95f65f51bb0819c0220a11fde0baba207f443957
Xavi Moreno <xaviermorenoaviles@gmail.com>
2017-06-27 23:37:38 -0500

    update array and foreach per persons

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
     echo "hola mundo";
     echo "estoy trabajando con un repositorio";
+
+    $datos = array('xavier','pepe');
+
+    foreach($datos as $key){
+        echo $key;
+    }
 ?>
